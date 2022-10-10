a3e6bc92a0cf8bea1b1b5ce8d2652ff21ae66383
VTem <11719850+vtem@user.noreply.gitee.com>
2022-10-10 17:38:25 +0800

    Changes

--- pages/suggest/suggest.vue
+++ pages/suggest/suggest.vue
@@ -4,7 +4,7 @@
 		<view class="title"><h2>意见和建议</h2></view>
 		<editor class="input" maxlength="200" placeholder="请输入您的意见或建议" style="border: 0.5px solid black;"/>
 		<button size="mini" class="submit">提交</button>
-		<navigator url="historysuggest/historysuggest">历史建议</navigator>
+		<navigator url="historysuggest/historysuggest" class="historysuggest">历史建议</navigator>
 	</view>
 </template>
 
@@ -26,6 +26,7 @@
 	.suggest{
 		text-align: center;
 		margin-top: 150rpx;
+		position: absolute;
 	}
 	.input{
 		margin-left:130rpx;
@@ -34,13 +35,27 @@
 		border-radius: 20px;
 		cursor: text;
 		overflow: auto;
+		margin-top: 250px;
+		position: absolute;
+		margin-top: 150rpx;
 	}
 	.submit{
-		margin-top: 10px;
+		margin-top: 880rpx;
+		margin-left: 325rpx;
 	}
 	.top{
-		width: 300rpx;
+		width: 400rpx;
 		height: 200rpx;
-		margin-bottom: 850rpx;
+		margin-bottom: 950rpx;
+		position: absolute;
+		margin-left: -50rpx;
+		margin-top: -150rpx;
+	}
+	.historysuggest{
+		margin-left: 325rpx;
+	}
+	.title{
+		position: absolute;
+		margin-left: 300rpx;
 	}
 </style>
