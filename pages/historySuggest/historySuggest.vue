c6d62d4a04307df5ac1cafd9de1f4251d4cbff29
VTem <11719850+vtem@user.noreply.gitee.com>
2022-10-11 14:50:32 +0800

    Changes

---
+++ pages/historySuggest/historySuggest.vue
@@ -0,0 +1,34 @@
+<template>
+	<view>
+		<view class="title"><h1>历史建议</h1></view>
+		<button class="select" size="mini">查询</button>
+		<navigator class="return" open-type="navigateBack">返回</navigator>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.select{
+		margin-top: 600rpx;
+		margin-left: 320rpx;
+	}
+	.title{
+		text-align: center;
+	}
+	.return{
+		text-align: center;
+		margin-top: 50rpx;
+	}
+</style>
---
+++ unpackage/dist/dev/mp-weixin/pages/historySuggest/historySuggest.js
@@ -0,0 +1,13 @@
+"use strict";
+var common_vendor = require("../../common/vendor.js");
+const _sfc_main = {
+  data() {
+    return {};
+  },
+  methods: {}
+};
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return {};
+}
+var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "E:/\u5F00\u53D1\u5DE5\u5177/HBuilderX/workspace/wupingchengtou/pages/historySuggest/historySuggest.vue"]]);
+wx.createPage(MiniProgramPage);
