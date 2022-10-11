6f4e6e968bd8af350fb5c2781edce8649b1ef86e
VTem <11719850+vtem@user.noreply.gitee.com>
2022-10-11 14:49:35 +0800

    Changes

--- pages/suggest/suggest.vue
+++ pages/suggest/suggest.vue
@@ -1,10 +1,10 @@
 <template>
 	<view class="suggest">
 		<image src="/static/`PFBIVJ3D(9FWY1YR(SUE]8.png" class="top" mode="aspectFit"></image>
-		<view class="title"><h2>意见和建议</h2></view>
+		<view class="title" v-html="code"></view>
 		<editor class="input" maxlength="200" placeholder="请输入您的意见或建议" style="border: 0.5px solid black;"/>
-		<button size="mini" class="submit">提交</button>
-		<navigator url="historysuggest/historysuggest">历史建议</navigator>
+		<button size="mini" class="submit" v-on:click="OnClick">{{submit}}</button>
+		<navigator url="../historySuggest/historySuggest" class="history">历史建议</navigator>
 	</view>
 </template>
 
@@ -12,11 +12,15 @@
 	export default {
 		data() {
 			return {
-				
+				code:"<h2>意见和建议</h2>",
+				submit:"提交"
 			}
 		},
-		methods: {
-			
+		methods:{
+			OnClick:function submit(){
+				this.submit="提交成功";
+				
+			}
 		}
 	}
 </script>
@@ -28,19 +32,37 @@
 		margin-top: 150rpx;
 	}
 	.input{
+		margin-top: 100rpx;
 		margin-left:130rpx;
 		width: 500rpx;
 		height: 700rpx;
 		border-radius: 20px;
 		cursor: text;
 		overflow: auto;
+		position: absolute;
 	}
 	.submit{
-		margin-top: 10px;
+		margin-top: 430px;
+		position: absolute;
+		margin-left: -50rpx;
 	}
 	.top{
 		width: 300rpx;
 		height: 200rpx;
 		margin-bottom: 850rpx;
+		margin-top: -180rpx;
+		position: absolute;
+		margin-left: -160rpx;
+	}
+	.title{
+		margin-top: 0rpx;
+		margin-left: 265rpx;
+		text-align: center;
+		position: absolute;
+	}
+	.history{
+		position: absolute;
+		margin-top: 940rpx;
+		margin-left: 320rpx;
 	}
 </style>
--- unpackage/dist/dev/mp-weixin/pages/suggest/suggest.js
+++ unpackage/dist/dev/mp-weixin/pages/suggest/suggest.js
@@ -3,14 +3,21 @@ var common_vendor = require("../../common/vendor.js");
 const _sfc_main = {
   data() {
     return {
-      code: "<h2>\u610F\u89C1\u548C\u5EFA\u8BAE</h2>"
+      code: "<h2>\u610F\u89C1\u548C\u5EFA\u8BAE</h2>",
+      submit: "\u63D0\u4EA4"
     };
   },
-  methods: {}
+  methods: {
+    OnClick: function submit() {
+      this.submit = "\u63D0\u4EA4\u6210\u529F";
+    }
+  }
 };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return {
-    a: $data.code
+    a: $data.code,
+    b: common_vendor.t($data.submit),
+    c: common_vendor.o((...args) => $options.OnClick && $options.OnClick(...args))
   };
 }
 var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "E:/\u5F00\u53D1\u5DE5\u5177/HBuilderX/workspace/wupingchengtou/pages/suggest/suggest.vue"]]);
